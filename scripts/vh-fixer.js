35b9fa41a3f6239196174846aeb28f253f46c91d
tompkinsfarmmusic <tompkinsfarmhand@gmail.com>
2022-05-12 12:11:11 -0400

    removed excess files/folders from dir tree, separated vh-fixer from html, minified everything, moved minified main code to index

---
+++ scripts/vh-fixer.js
@@ -0,0 +1,7 @@
+var vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
